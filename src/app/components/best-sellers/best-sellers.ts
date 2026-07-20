c53be2e329f37a47fae9a73e5f1af98d6ef00b99
Rushikesh <hrishi210402@gmail.com>
2026-07-20 13:15:00 +0530

    routerlink

--- src/app/components/best-sellers/best-sellers.ts
+++ src/app/components/best-sellers/best-sellers.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../services/product';
 import { ProductCard } from "../product-card/product-card";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-best-sellers',
-  imports: [ProductCard],
+  imports: [ProductCard, RouterLink],
   templateUrl: './best-sellers.html',
   styleUrl: './best-sellers.css',
 })
